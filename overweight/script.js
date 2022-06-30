c97cc81d055d46bc28060469994676c5a4efed70
ashenragee <roolywan4igg@gmail.com>
2022-06-30 11:55:19 +0300

    return to previous version

--- overweight/script.js
+++ overweight/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:script.js
 const userWeight = prompt('What is your weight?(kg)');
 const userHight = prompt('What is your hight?(m)');
 let userOverWeightIndex = userWeight / Math.pow(userHight, 2);
@@ -8,14 +7,3 @@ if (userOverWeightIndex >= 25) {
 } else {
 	alert("You aren't overweight, nice!");
 }
-=======
-const userWeight = prompt('What is your weight?(kg)');
-const userHight = prompt('What is your hight?(m)');
-let userOverWeightIndex = userWeight / Math.pow(userHight, 2);
-console.log(userOverWeightIndex);
-if (userOverWeightIndex >= 25) {
-	alert('You are overweight');
-} else {
-	alert("You aren't overweight");
-}
->>>>>>> 4839626d3cc4fe5681d676d3bd42424fa3131320:overweight/script.js
